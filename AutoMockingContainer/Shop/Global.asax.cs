eb26d903a02f5f239e0925ff20a6c254c72aabc2
altair961 <altair89@list.ru>
2020-12-06 21:49:11 +0100

    Basic configuration was performed

---
+++ AutoMockingContainer/Shop/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http;
+
+namespace Shop
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(ShopConfig.Register);
+        }
+    }
+}
